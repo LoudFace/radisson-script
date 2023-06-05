238fd51d03a6c116c6d30c9186cca381aa81ae1e
“samLoudface” <126796716+samLoudface@users.noreply.github.com>
2023-06-05 20:28:42 +0100

    all line chart plotted

--- src/newAppPage.ts
+++ src/newAppPage.ts
@@ -2,8 +2,9 @@
 //import fetch from 'node-fetch';
 //import Webflow from 'webflow-api';
 import Airtable from 'airtable';
+import * as echarts from 'echarts';
 
-import { appMonthlyChart, appRatingChart } from './newApppageChart';
+import { appMonthlyChart, appRatingChart, roomBookedChart } from './newApppageChart';
 //const webflowApi = process.env.API_KEY;
 
 const getColumnData = function (nameOfField: string, records) {
@@ -29,6 +30,11 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
+const convertoSingleValue = function (itemarray) {
+  const [a] = itemarray.slice(-1);
+  return numberWithCommas(a);
+};
+
 window.Webflow ||= [];
 window.Webflow.push(() => {
   console.log('samuel');
@@ -41,10 +47,33 @@ window.Webflow.push(() => {
     });
   };
 
+  ////////test bar chart
+
+  const active = document.getElementById('activeUsers');
+  const activeInit = echarts.init(active);
+
+  activeInit.setOption({
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+          color: 'rgba(180, 180, 180, 0.2)',
+        },
+      },
+    ],
+  });
+
   //////////////////////////airtable variables
-
   const tableMonthlyDownload = 'tblhxgDITpzlU0Nu4';
-
   getTableRecords(tableMonthlyDownload).eachPage(function page(records) {
     console.log(records);
     const months = getColumnData('Month', records);
@@ -84,11 +113,6 @@ window.Webflow.push(() => {
     );
     //getting the last item of the arrays
 
-    const convertoSingleValue = function (itemarray) {
-      const [a] = itemarray.slice(-1);
-      return numberWithCommas(a);
-    };
-
     const tDownload = convertoSingleValue(totalDownloads);
     const iOsLatest = convertoSingleValue(iOs);
     const androidLatest = convertoSingleValue(android);
@@ -113,6 +137,7 @@ window.Webflow.push(() => {
         Wowvalue > 0 ? 'green' : 'red'
       }"> ${Wowvalue}% </span>MoM</div>`;
     };
+
     updateKey(totalDownloadsKeyWrap, tDownload, tDownloadWoW);
     updateKey(iosKeyWrap, iOsLatest, iOsLatestMoM);
     updateKey(androidKeyWrap, androidLatest, androidMomLatest);
@@ -138,12 +163,60 @@ window.Webflow.push(() => {
     /////////passing Data to the imported function
     appRatingChart(months, android, ios, androidYoYFormated, iosYoyFormated);
 
-    //////// UI update
+    //////// UI update for the
     const [iosYoyLatest] = iosYoyFormated.slice(-1);
     const [androidYoyLatest] = androidYoYFormated.slice(-1);
     const iosWrap = (document.querySelector('[rd-element="iosData"]').textContent = iosYoyLatest);
     const androidWrap = (document.querySelector('[rd-element="androidData"]').textContent =
       androidYoyLatest);
+  });
+
+  /////////////Room night Chart
+  /// Room Nights iOS Android
+  const revenueTable = 'tblT1C7g9L0pJGYT5';
+  getTableRecords(revenueTable).eachPage(function page(records) {
+    console.log(records);
+    const months = getColumnData('Month', records);
+    const androidRns = getColumnData('Android RNs', records);
+    const iosRns = getColumnData('iOS RNs', records);
+
+    //////get MoM data
+    const androidRnsMoM = getColumnData('Android RNs (MoM)', records);
+    const iosRnsMoM = getColumnData('iOS RNs (MoM)', records);
+    const androidRnsMoMFormated = formatColumnsTOPercent(androidRnsMoM);
+    const iosRnsMoMFormated = formatColumnsTOPercent(iosRnsMoM);
+    //console.log(androidRnsMoMFormated, iosRnsMoMFormated);
+
+    roomBookedChart(months, androidRns, iosRns, androidRnsMoMFormated, iosRnsMoMFormated);
+
+    ////Update the UI Keys
+    const [androidMomFormatedLatest] = androidRnsMoMFormated.slice(-1);
+    const [iosRnsMoMFormatedLatest] = iosRnsMoMFormated.slice(-1);
+
+    const iosRnsLatest = convertoSingleValue(iosRns);
+    const androidRnsLatest = convertoSingleValue(androidRns);
+    console.log(androidRnsLatest);
+
+    const androidWrap = document.querySelector('[rd-element="androidRoomnight"]');
+    const iosWrap = document.querySelector('[rd-element="iosRoomnight"]');
+    console.log(iosWrap);
+
+    const updateKey = function (contWrap, amount, Wowvalue) {
+      contWrap.innerHTML = `<div class="bookings-number-text ligth--text-grad">${amount}</div><div class="key--percent"><span class="key__span--text ${
+        Wowvalue > 0 ? 'green' : 'red'
+      }"> ${Wowvalue}% </span>MoM</div>`;
+    };
+
+    updateKey(iosWrap, iosRnsLatest, iosRnsMoMFormatedLatest);
+    updateKey(androidWrap, androidRnsLatest, androidMomFormatedLatest);
+  });
+
+  /////////////////
+  //////////Bar chart
+  ////Active users Benchmark tabele
+  const activeTableId = 'tblBB9s2JT4wbCYAP';
+  getTableRecords(activeTableId).eachPage(function page(records) {
+    console.log(records);
   });
 
   //Working method Querry
--- src/newApppageChart.ts
+++ src/newApppageChart.ts
@@ -2,10 +2,18 @@ import * as echarts from 'echarts';
 
 const monthlyChartWrapper = document.getElementById('monthChart');
 const appRatingWrap = document.getElementById('appRating');
+const roomBooked = document.getElementById('roomBookInstance');
+
+/////////////////
+//////////Barchart wrapper
 
 //Initialize monthly chart
 const monthlyChart = echarts.init(monthlyChartWrapper);
 const appRating = echarts.init(appRatingWrap);
+const roomBookedInstance = echarts.init(roomBooked);
+
+//////////////
+//////////// Bar chart
 
 /////Monthly Downloads chart
 export const appMonthlyChart = function (
@@ -286,7 +294,7 @@ export const appMonthlyChart = function (
   });
 };
 
-/////
+///// App rating chart
 export const appRatingChart = function (
   xAxis: 0,
   yAxis: 0,
@@ -296,7 +304,7 @@ export const appRatingChart = function (
 ) {
   appRating.setOption({
     grid: {
-      width: '80%',
+      width: '90%',
       left: 40,
     },
     color: ['#FAACA8', '#9EEDFE', 'transparent', 'transparent'],
@@ -453,6 +461,180 @@ export const appRatingChart = function (
         start: 5,
         end: 100,
         minSpan: 20,
+      },
+    ],
+  });
+};
+
+///// roomBookedChart
+export const roomBookedChart = function (
+  xAxis: 0,
+  yAxis: 0,
+  yAxis2: 0,
+  yaxisPercent: 0,
+  yaxisPercent2: 0
+) {
+  roomBookedInstance.setOption({
+    grid: {
+      width: '90%',
+      left: 40,
+    },
+    color: ['#FAACA8', '#9EEDFE', 'transparent', 'transparent'],
+    title: {
+      show: false,
+      text: 'Radisson Playbook chart instance',
+    },
+    tooltip: {
+      trigger: 'axis',
+      backgroundColor: '#333333',
+      borderColor: '#333333',
+      padding: 20,
+      textStyle: {
+        color: ' #CCCCCC',
+        fontSize: 10,
+      },
+      axisPointer: {
+        // type: 'cross',
+        label: {
+          backgroundColor: 'red',
+        },
+      },
+      formatter: function (params) {
+        const ic1 = `<span data-tooltip="minimum" style="border-radius:2px; text-align: left; background-color:${params[0].color}; display:inline-block; height: 12px; width:12px; margin-right: 5px;"></span>`;
+        const ic2 = `<span data-tooltip="minimum" style="border-radius:2px; text-align: left; background-color:${params[1].color}; display:inline-block; height: 12px; width:12px; margin-right: 5px;"></span>`;
+        const title = `<span style=" text-align: left; color: white; border-bottom: 1px solid #4D4D4D; margin-bottom: 10px; padding-bottom:5px; display: inline-block; width:100%;"> ${params[0].name} </span>`;
+
+        const [, , instantPercent, rfpbookings] = params;
+        const percentInstant = ` <span><span style="color:${
+          instantPercent.data > 0 ? '#17B96B' : '#FE4B36'
+        }; ">${instantPercent.data}% </span> MoM</span> `;
+        const percentRfp = ` <span><span style="color:${
+          rfpbookings.data > 0 ? '#17B96B' : '#FE4B36'
+        }; ">${rfpbookings.data}% </span> MoM</span> `;
+
+        // const formatedTooltip = `<div style= "display: flex; align-items: center; justify-content: space-between; gap: 1rem;"> <div> ${ic1} ${params[0].seriesName}</div> : ${params[0].data} ${percentincrease1} </div>`;
+
+        const formatedTooltip = ` <div  style= "display: flex; align-items: center; justify-content: space-between; gap: 1rem;"> <div> ${ic1} ${params[0].seriesName}</div> : ${params[0].data} ${percentInstant} </div>`;
+        const formatedTooltipRfp = `<div style= "display: flex; align-items: center; justify-content: space-between; gap: 1.5rem;" > <div> ${ic2} ${params[1].seriesName}</div> : ${params[1].data} ${percentRfp} </div>`;
+
+        return `${title} <br />
+                 ${formatedTooltip}  
+                  ${formatedTooltipRfp}`;
+      },
+    },
+    xAxis: {
+      // gridIndex: 1,
+
+      type: 'category',
+      boundaryGap: false,
+      axisLine: {
+        //show: true,
+        onZero: false,
+        show: true,
+
+        lineStyle: {
+          color: 'white',
+          width: 2,
+        },
+      },
+      axisLabel: {
+        color: 'white',
+        //interval: 1,
+        padding: [10, 0, 0, 0],
+      },
+      data: xAxis,
+    },
+    yAxis: {
+      splitLine: {
+        show: true,
+        lineStyle: {
+          width: 0.5,
+          color: '#262626',
+        },
+      },
+      type: 'value',
+      axisLabel: {
+        formatter: function (value) {
+          const num = value / 1000;
+          return `${num}k`;
+        },
+      },
+      axisLine: {
+        show: true,
+        color: 'white',
+        width: 2,
+        lineStyle: {
+          color: 'white',
+          width: 2,
+          cap: 'round',
+        },
+      },
+      min: 0,
+      // max: 0,
+    },
+    series: [
+      {
+        name: 'Android',
+        type: 'line',
+        lineStyle: {
+          //width: 0.5,
+          // color: 'red',
+        },
+        areaStyle: {
+          opacity: 0.8,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: '#FAACA8',
+            },
+            {
+              offset: 0.6,
+              color: 'transparent',
+            },
+          ]),
+        },
+        showSymbol: false,
+        data: yAxis,
+      },
+      {
+        name: 'iOs',
+        type: 'line',
+        showSymbol: false,
+        areaStyle: {
+          opacity: 0.2,
+          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: '#9EEDFE',
+            },
+            {
+              offset: 1,
+              color: 'transparent',
+            },
+          ]),
+        },
+        data: yAxis2,
+      },
+      {
+        name: 'androidPercent',
+        type: 'line',
+        showSymbol: false,
+        data: yaxisPercent,
+      },
+      {
+        name: 'iosPercent',
+        type: 'scatter',
+        showSymbol: false,
+        data: yaxisPercent2,
+      },
+    ],
+    dataZoom: [
+      {
+        type: 'inside',
+        xAxisIndex: [0, 1],
+        start: 5,
+        end: 100,
+        minSpan: 10,
       },
     ],
   });
